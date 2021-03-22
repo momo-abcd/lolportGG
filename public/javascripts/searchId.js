3cae70f0f4c1d5491a14901d35c406acd8c3029a
momo-abcd <yym4481@naver.com>
2021-03-22 20:17:02 +0900

    second commit

---
+++ public/javascripts/searchId.js
@@ -0,0 +1,10 @@
+// const getUsers = () => {
+//     axios
+//       .get("https://www.naver.com")
+//       .then((response) => {
+//         const users = response
+//         console.log('respone: ',users);
+//       })
+//       .catch((error) => console.error('error'));
+//   };
+//   getUsers();
--- routes/report.js
+++ routes/report.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var axios = require('axios');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -7,4 +8,22 @@ router.get('/', function(req, res, next) {
 //   res.render('index', { title: 'Express' });
 });
 
+// router.get('/searchID', function(req, res, next) {
+//     res.render('report');
+// //   res.render('index', { title: 'Express' });
+// });
+
+router.post('/searchId/', function(req, res, next) {
+    // console.log(req);
+    res.send(req.body.select[0]);
+
+
+    // riot api module
+    // axios.post('https://naver.com')
+    // .then(resp => {
+    //     res.send(resp.data);
+    // }).catch(err => {
+    //     console.log(err);
+    // });
+});
 module.exports = router;
